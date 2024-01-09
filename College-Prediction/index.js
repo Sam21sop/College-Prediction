c3f9c7ad22305a795c8e13a5b7acf4f67a165588
Sam21sop <sbussiness21@gmail.com>
2024-01-09 16:59:38 +0530

    express server setup

---
+++ College-Prediction/index.js
@@ -0,0 +1,36 @@
+// Importing the Express module
+import express from 'express';
+import path from 'path';
+
+
+// Creating an instance of the Express application
+const server = express();
+
+// set EJS as the view engine
+server.set('view engine', 'ejs');
+
+// Specify directory path
+const dirPath = path.resolve(path.join('College-Prediction', 'src', 'views'));
+
+// set the directory name
+server.set('views', dirPath);
+
+// Define a route
+server.get('/', (req, res) => {
+    const data = {
+        title: "College Prediction",
+        message: "College For Career"
+    };
+
+    // render index template
+    res.render('index', data);
+});
+
+
+
+
+
+
+
+
+export default server;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-import express from 'express'
+import dotenv from 'dotenv';
+dotenv.config(); 
 
-const server = express();
+// import server instance from index module
+import server from "./College-Prediction/index.js";
 
-server.get('/', (req, res)=> {
-    res.send("Welcome to college prediction")
+const PORT = process.env.PORT | 8080;
+
+// Server is listening on 8080
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-server.listen(8080, ()=>{
-    console.log("server listen on 8080...");
-})
