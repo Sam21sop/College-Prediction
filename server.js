73d03b6d429c665509f990946f965e95711ae3dc
Sam21sop <sbussiness21@gmail.com>
2024-02-09 15:06:53 +0530

    db config

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ import server from "./College-Prediction/index.js";
 import connectToDB from "./College-Prediction/NodeJs-Code/config/mongooseConfig.js";
 
 
-const PORT = processEnvVar.PORT | 8080;
-const BASE_URL = processEnvVar.DB_URL;
+const PORT = processEnvVar.PORT || 8080;
+const BASE_URL = processEnvVar.DB_BASE_URL;
+const DB_NAME = processEnvVar.DB_NAME;
+
 
 // Server is listening on 8080
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-  connectToDB(BASE_URL);
+  connectToDB(BASE_URL, DB_NAME);
 });
