d16548f08d22ae74f0b5cbaab776c073dff57086
Sam21sop <sbussiness21@gmail.com>
2024-02-09 15:07:25 +0530

    routes add

--- College-Prediction/index.js
+++ College-Prediction/index.js
@@ -1,6 +1,7 @@
 // Importing the Express module
 import express from 'express';
 import studentRouter from './NodeJs-Code/routes/studentRoutes.js';
+import collegeRouter from './NodeJs-Code/routes/collegeRoutes.js';
 
 
 // Creating an instance of the Express application
@@ -8,6 +9,11 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.send("Hello Student..!")
+})
+
 server.use('/api/student', studentRouter)
+server.use('/api/college', collegeRouter)
 
 export default server;
