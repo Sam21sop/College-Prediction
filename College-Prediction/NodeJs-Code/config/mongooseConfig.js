b65ae6239d98a1723547ac47bea50b10f527d6c8
Sam21sop <sbussiness21@gmail.com>
2024-02-09 15:08:35 +0530

    db config

--- College-Prediction/NodeJs-Code/config/mongooseConfig.js
+++ College-Prediction/NodeJs-Code/config/mongooseConfig.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 // function to connect mongodb using mongoose
-const connectToDB = async (baseURL) => {
+const connectToDB = async (BASE_URL, DB_NAME) => {
     try {
-        await mongoose.connect(baseURL);
+        await mongoose.connect(`mongodb://${BASE_URL}/${DB_NAME}`);
+        // await mongoose.connect(`mongodb://127.0.0.1:27017/CollegePrediction`);
         console.log("MongoDB Connected using Mongoose");
     } 
     catch (error) {
